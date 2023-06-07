6a47ec6a983b84308861b71679962b984d82c23a
ELASHMAWYDEV <elashmawydev@gmail.com>
2023-06-08 02:12:43 +0300

    added empty order screen file

---
+++ lib/ui/screens/order_history/order_history_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OrderHistory extends StatefulWidget {
+  const OrderHistory({super.key});
+
+  @override
+  State<OrderHistory> createState() => _OrderHistoryState();
+}
+
+class _OrderHistoryState extends State<OrderHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Center(
+        child: Text("Order History Screen"),
+      )),
+    );
+  }
+}
